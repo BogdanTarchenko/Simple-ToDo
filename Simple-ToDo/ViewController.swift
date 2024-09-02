1c3b9159ffa6c7c91120aff9cfbaa2c40577ae81
BogdanTarchenko <tarchenko.bogdan@outlook.com>
2024-09-02 21:01:08 +0700

    Initial Commit

---
+++ Simple-ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple-ToDo
+//
+//  Created by Богдан Тарченко on 02.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
